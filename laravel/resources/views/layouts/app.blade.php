a67ac9722b263ee909f315af422713bc934c5986
FANDIL PERISWANTO <periswanto@gmail.com>
2026-07-02 13:32:38 +0700

    add app.blade.php sebagai Layouts/shared

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!-- Halaman ini berfungsi seperti Layouts/shared -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Attendance System</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <span class="navbar-brand">
+            Employee Attendance System
+        </span>
+    </div>
+</nav>
+
+<div class="container mt-4">
+
+    @yield('content')
+
+</div>
+
+<!-- <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <span class="navbar-brand">
+            Fandil - 2026
+        </span>
+    </div>
+</nav> -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
