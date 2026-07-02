2c5669ef9d4488b1a87527e6c3c74ee1563c2cae
FANDIL PERISWANTO <periswanto@gmail.com>
2026-07-02 11:42:34 +0700

    change web.php to "Hello2 Fandil:"

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    #return view('welcome');
+    return "Hello2 Fandil";
 });
+
+// Route::get('/', function () {
+//     return "Hello Fandil";
+// });
